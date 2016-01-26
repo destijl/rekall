b3d341a88bffa416087d1159fdc0d72ef8e1a95b
destijl <github@mailgreg.com>
2016-01-26 13:06:17 -0800

    Bypass rekall setup.py and install rekall-core.

--- setup.py
+++ setup.py
@@ -20,27 +20,108 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-"""Meta-script for pulling in all Rekall components."""
-
+"""Installation and deployment script."""
 __author__ = "Michael Cohen <scudette@gmail.com>"
 
+import os
+import platform
+import subprocess
 import versioneer
 
 try:
-    from setuptools import setup
+    from setuptools import find_packages, setup, Command
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import find_packages, setup
 
 rekall_description = "Rekall Memory Forensic Framework"
 
-MY_VERSION = versioneer.get_version()
+current_directory = os.path.join(os.path.dirname(__file__), "rekall-core")
+os.chdir("rekall-core")
+
+
+def find_data_files_directory(source):
+    result = []
+    for directory, _, files in os.walk(source):
+        files = [os.path.join(directory, x) for x in files]
+        result.append((directory, files))
+
+    return result
+
+# These versions are fixed to the exact tested configuration. Prior to release,
+# please use "setup.py pip_upgrade" to test with the latest version. This
+# approach ensures that any Rekall version will always work as tested - even
+# when external packages are upgraded in an incompatible way.
+install_requires = [
+    "PyAFF4 >= 0.20",
+    "PyYAML >= 3.11",
+    "acora >= 1.9",
+    "argparse >= 1.2.1",
+    "arrow >= 0.7.0",
+    "capstone >= 3.0.3",
+    "efilter == 1450268920",
+    "intervaltree >= 2.1.0",
+    "pycrypto >= 2.6.1",
+    "pyelftools >= 0.23",
+    "pytz >= 2015.7",
+    "sortedcontainers >= 1.4.2",
+]
+
+if platform.system() == "Windows":
+    install_requires.append("pypiwin32 == 219")
+    install_requires.append("capstone-windows >= 3.0.3")
+
+
+class PIPUpgrade(Command):
+    description = "Upgrade all the dependencies in the current virtualenv."
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        required = [x.split()[0] for x in install_requires]
+        output = subprocess.check_output(
+            ["pip", "install", "--upgrade"] + required)
+
+        # Print the current versions.
+        output = subprocess.check_output(
+            ["pip", "freeze"])
+
+        result = []
+        for package in required:
+            result.append(
+                [x for x in output.splitlines()
+                 if package in x][0])
+
+        print "\n".join(sorted(result))
+
+
+class CleanCommand(Command):
+    description = ("custom clean command that forcefully removes "
+                   "dist/build directories")
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        if os.getcwd() != self.cwd:
+            raise RuntimeError('Must be in package root: %s' % self.cwd)
+
+        os.system('rm -rf ./build ./dist')
+
+commands = versioneer.get_cmdclass()
+commands["pip_upgrade"] = PIPUpgrade
+commands["clean"] = CleanCommand
 
 setup(
-    name="rekall",
-    version=MY_VERSION,
-    cmdclass=versioneer.get_cmdclass(),
+    name="rekall-core",
+    version=versioneer.get_version(),
+    cmdclass=commands,
     description=rekall_description,
-    long_description=open("README.md").read(),
+    long_description=open(os.path.join(current_directory, "README.rst")).read(),
     license="GPL",
     url="https://www.rekall-forensic.com/",
     author="The Rekall team",
@@ -51,11 +132,20 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
+    scripts=["rekall/rekal.py"],
+    package_dir={'rekall': 'rekall'},
+    packages=find_packages('.'),
+    include_package_data=True,
+    data_files=(
+        find_data_files_directory('resources')
+    ),
+    entry_points="""
+    [rekall.plugins]
+    plugins=rekall.plugins
 
-    # This requires an exact version to ensure that installing the meta package
-    # pulls in tested dependencies.
-    install_requires=[
-        "rekall-core >= 1.4.0.pre3",
-        "rekall-gui >= 1.4.0.pre3",
-    ],
+    [console_scripts]
+    rekal = rekall.rekal:main
+    rekall = rekall.rekal:main
+    """,
+    install_requires=install_requires,
 )
